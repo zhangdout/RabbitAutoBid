f04e9a928cfde6672d677e08e8ba544b8ff0857f
Yichi (Evan) Zhang <evanzhang0209@gmail.com>
2025-02-17 00:40:31 +1100

    Register Db connection

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,15 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
