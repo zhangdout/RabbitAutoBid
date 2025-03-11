a2aaca3830ad1464d3883a2c268f21a93cd7c981
Yichi (Evan) Zhang <evanzhang0209@gmail.com>
2025-03-11 22:10:28 +1100

    Initiate Yarp reverse proxy

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+app.MapReverseProxy();
+
+app.Run();
