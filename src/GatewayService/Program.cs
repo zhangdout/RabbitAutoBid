134b10b2657b573c92c751a6ee450f6aab80fa57
Yichi (Evan) Zhang <evanzhang0209@gmail.com>
2025-03-12 14:03:48 +1100

    Complete gateway service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        //API 会验证 Token 是否由 IdentityServiceUrl的地址 签发
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+    
+var app = builder.Build();
+
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
