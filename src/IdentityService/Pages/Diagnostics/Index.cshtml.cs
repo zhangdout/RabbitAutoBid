d1b994674ad337e2a9e67e2f9e66a152db3592a4
Yichi (Evan) Zhang <evanzhang0209@gmail.com>
2025-03-12 23:00:53 +1100

    Add identity-svc to docker-compose file

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -17,6 +17,7 @@ public class Index : PageModel
     public async Task<IActionResult> OnGet()
     {
         var localAddresses = new List<string?> { "127.0.0.1", "::1" };
+        /*
         if(HttpContext.Connection.LocalIpAddress != null)
         {
             localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
@@ -26,6 +27,7 @@ public class Index : PageModel
         {
             return NotFound();
         }
+        */
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
             
